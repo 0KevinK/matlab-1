7016225e71d8279817f4dd3f0dd15bdab3934393
0KevinK <33936552+0KevinK@users.noreply.github.com>
2017-11-26 13:48:24 +0100

    Update main.m

--- main.m
+++ main.m
@@ -6,15 +6,29 @@ function choice = main();
     meny();
     degree=[];
     points=[];
+    coeffs=[];
     while true
         choice = input('\nVälj ett menyval')
         if choice == 1
-            askdegree();
+            degree= askdegree();
         elseif choice == 2
-            askpoints();
+            points=askpoints();
         end
-    if ~isempty(degree) && ~isempty(points)
         if choice == 3
+            if ~isempty(degree) || ~isempty(points)
+           coeffs = findcoeffs(degree,points)
+            else
+        disp('Gradtal eller punter är inte definerat.')
+       
+            end
         end
+    if ~isempty(degree) && ~isempty(points) && ~isempty(coeffs)
+         if choice == 4
+             plotpoly(coeffs,points);
+        end
+        if choice == 5
+             polyroots(coeffs);
+        end
+             
     end
 end
